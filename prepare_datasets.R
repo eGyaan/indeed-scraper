8bdb1af1c2178a7af065209a3a621f16fd5b5a9f
Everet Rummel <erummel93@gmail.com>
2018-01-12 17:32:07 -0500

    Formatting.

--- prepare_datasets.R
+++ prepare_datasets.R
@@ -3,7 +3,8 @@
 # Scrape data from search of "Data Scientist" near NYC.
 data_sci = scrape_indeed(
   base_url = 'https://www.indeed.com/jobs?q=data+scientist&l=new+york%2C+ny',
-  num_pages = 5)
+  num_pages = 5
+)
 
 # Scrape data from search of "Data Analyst" near NYC.
 data_ana = scrape_indeed(
@@ -18,12 +19,13 @@ data_sci_2 = data_sci %>%
     grepl(pattern = 'data scientist', x = title, ignore.case = TRUE),
     # Remove cases where job title contains "analyst" or seniority level.
     !grepl(pattern = 'analyst|sr.|jr.|senior|junior|lead|intern', 
-           x = title, ignore.case = TRUE)) 
+           x = title, ignore.case = TRUE)
+  ) 
 
 data_ana_2 = data_ana %>%
   filter(
     grepl(pattern = 'data analyst', x = title, ignore.case = TRUE),
     !grepl(pattern = 'scientist|sr.|jr.|senior|junior|lead|intern', 
-           x = title, ignore.case = TRUE)) 
+           x = title, ignore.case = TRUE)
+  ) 
 
-  
